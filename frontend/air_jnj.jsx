3dfb3d60e47f0f73a99c37b64eb016944cdb0011
Jay Lam <lamjay415@gmail.com>
2021-08-02 11:23:03 -0700

    created actions and reducers for user auth

--- frontend/air_jnj.jsx
+++ frontend/air_jnj.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { login, logout, signup } from './util/session_api_util'
 
 document.addEventListener("DOMContentLoaded", ()=>{
     ReactDOM.render(<h1>Welcome to AirJnJ</h1>, document.getElementById('root'));
+    window.signup = signup;
+    window.login = login;
+    window.logout = logout;
 });
