d30e5266dbd6a3e4ad65e10e1b41aef3108e50ac
Jay Lam <lamjay415@gmail.com>
2021-08-02 01:33:07 -0700

    created backend auth logic

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,37 @@
 class User < ApplicationRecord
+
+    validates :email, :password_digest, :session_token, presence: true
+    validates :email, uniqueness: true
+    validates :password, length: {minimum: 6, allow_nil: true}
+
+    def self.find_by_credentials(email, password)
+        @user = User.find_by(email: email)
+        if @user && @user.is_password?(password)
+            @user
+        else
+            nil
+        end
+    end
+
+    attr_reader :password
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(password_digest).is_password?(password)
+    end
+
+    def reset_session_token!
+        self.session_token = SecureRandom.urlsafe_base64
+        self.save
+        self.session_token
+    end
+
+    after_initialize :ensure_session_token
+    def ensure_session_token
+        self.session_token ||= SecureRandom.urlsafe_base64
+    end
+    
 end
