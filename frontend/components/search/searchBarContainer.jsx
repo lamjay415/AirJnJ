35f3bfc7d04299c2020bc20b81113512b78ba091
Jay Lam <lamjay415@gmail.com>
2021-08-10 12:09:54 -0700

    updated api key encrypting

--- frontend/components/search/searchBarContainer.jsx
+++ frontend/components/search/searchBarContainer.jsx
@@ -20,12 +20,27 @@ class SearchBar extends React.Component {
         this.props.history.push(`search/${this.state.searchLocation}`);
     }
 
+    componentDidMount(){
+        let autocomplete;
+        function initAutoComplete(){
+            autocomplete = new google.maps.places.Autocomplete(
+                document.getElementById('autocomplete'),
+                {
+                    types: ['city'],
+                    componentRestrictions: {'country': ['USA']},
+                    fields: ['place_id', 'geometry', 'name']
+                }
+            );
+            autocomplete.addListener('place_changed', onPlaceChanged);
+        }
+    }
+
     render(){
         return (
             <div className='searchBar-container'>
                 <div className='search-div'>
                     <div className='search-text'>Location </div>
-                    <input type='text' onChange={this.update} placeholder='Where are you going?' className='search-input'/>
+                    <input type='text' onChange={this.update} placeholder='Where are you going?' className='search-input' id='autocomplete'/>
                 </div>
                 <div className='search-button' onClick={this.handleClick}>
                     Search
